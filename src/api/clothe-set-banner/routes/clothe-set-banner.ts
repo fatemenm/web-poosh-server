bc25af0b06d86cbf9a031fc028a0b526511b3265
Fateme Noori Moghadam <fatemenm058@gmail.com>
2024-11-19 21:30:54 +0330

    feat: add clotheSetBanner entity

---
+++ src/api/clothe-set-banner/routes/clothe-set-banner.ts
@@ -0,0 +1,7 @@
+/**
+ * clothe-set-banner router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::clothe-set-banner.clothe-set-banner');
---
+++ src/api/clothe-set-banner/services/clothe-set-banner.ts
@@ -0,0 +1,7 @@
+/**
+ * clothe-set-banner service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::clothe-set-banner.clothe-set-banner');
