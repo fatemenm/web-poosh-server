2fb272260fee8b407a15f180c2b3377c532b86fd
Fateme Noori Moghadam <fatemenm058@gmail.com>
2025-01-23 07:00:16 +0330

    feat: add filters (component), sizeGuideImage, sizeTable, and careTips properties

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,40 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Struct, Schema } from '@strapi/strapi';
+
+export interface ProductColorImages extends Struct.ComponentSchema {
+  collectionName: 'components_product_color_images';
+  info: {
+    displayName: 'colorImages';
+    icon: 'picture';
+  };
+  attributes: {
+    color: Schema.Attribute.String & Schema.Attribute.Required;
+    images: Schema.Attribute.Media<'images' | 'files', true> &
+      Schema.Attribute.Required;
+  };
+}
+
+export interface CategoryFilter extends Struct.ComponentSchema {
+  collectionName: 'components_category_filters';
+  info: {
+    displayName: 'Filter';
+    icon: 'filter';
+  };
+  attributes: {
+    type: Schema.Attribute.Enumeration<
+      ['availability', 'discount', 'size', 'color', 'style']
+    >;
+    query: Schema.Attribute.String;
+    value: Schema.Attribute.JSON;
+    images: Schema.Attribute.Media<'images' | 'files', true>;
+    index: Schema.Attribute.Integer;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'product.color-images': ProductColorImages;
+      'category.filter': CategoryFilter;
+    }
+  }
+}
