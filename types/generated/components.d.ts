715937a9afb4e2f55fb407f8754e2d193e1d77fa
Fateme Noori Moghadam <fatemenm058@gmail.com>
2025-02-05 17:18:24 +0330

    refactor: sort file

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,18 +1,5 @@
 import type { Struct, Schema } from '@strapi/strapi';
 
-export interface ProductColorImages extends Struct.ComponentSchema {
-  collectionName: 'components_product_color_images';
-  info: {
-    displayName: 'colorImages';
-    icon: 'picture';
-  };
-  attributes: {
-    color: Schema.Attribute.String & Schema.Attribute.Required;
-    images: Schema.Attribute.Media<'images' | 'files', true> &
-      Schema.Attribute.Required;
-  };
-}
-
 export interface CategoryFilter extends Struct.ComponentSchema {
   collectionName: 'components_category_filters';
   info: {
@@ -27,11 +14,24 @@ export interface CategoryFilter extends Struct.ComponentSchema {
   };
 }
 
+export interface ProductColorImages extends Struct.ComponentSchema {
+  collectionName: 'components_product_color_images';
+  info: {
+    displayName: 'colorImages';
+    icon: 'picture';
+  };
+  attributes: {
+    color: Schema.Attribute.String & Schema.Attribute.Required;
+    images: Schema.Attribute.Media<'images' | 'files', true> &
+      Schema.Attribute.Required;
+  };
+}
+
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
-      'product.color-images': ProductColorImages;
       'category.filter': CategoryFilter;
+      'product.color-images': ProductColorImages;
     }
   }
 }
